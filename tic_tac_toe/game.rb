90398bfaa1df61e0b4bccf89cc97dffdc1579c0e
Julian <artjulius@hotmail.com>
2019-08-30 14:06:09 -0500

    Version 1 Tic Tac Toe

--- tic_tac_toe/game.rb
+++ tic_tac_toe/game.rb
@@ -1,11 +1,14 @@
 class TicTacToe
     #attr_accessor :count
+    #$rol = nil
     def initialize (board)
         @board = board
-        @player
+        @rol
+        @draw
     end
     
     def print_board
+        puts "GOOD LUCK!"
         puts "\n #{@board[0]} | #{@board[1]} | #{@board[2]} "
         puts "-----------"
         puts " #{@board[3]} | #{@board[4]} | #{@board[5]} "
@@ -19,59 +22,192 @@ class TicTacToe
         puts "Which player do you prefer?"
         puts "1 for X or 2 for O"
         puts "==========================="
-        rol = gets.chomp.to_i
-        validate_rol (rol)
+        @rol = gets.chomp.to_i
+        validate_rol
 
-        puts "#{@player}, please enter the row:"
-        input_row = gets
-        return false 
+        #puts "#{@player}, please enter the row:"
+        #input_row = gets
+        #return false 
 
-         puts "#{@player}, please enter the column:"
-        input_column = gets
-        return false 
+        #puts "#{@player}, please enter the column:"
+        #input_column = gets
+        #return false 
 
-        (input_row, input_column)
-
-
-
-
+        #(input_row, input_column)
     end
     
-    def validate_rol (rol)
-        if rol == 1 || rol == 2
-            @player = rol
-
+    def validate_rol
+        if @rol == 1 || @rol == 2
+            #@player = rol
             print_board
+            play
         else
             puts "Invalid option"
             get_inputs
         end
     end
     
-    def valid_move?(input_row, input_column)
-        @board[input_row.-1][input_column.-1].strip.empty?
-       
-    end
+    #def valid_move?(input_row, input_column)
+    #    @board[input_row.-1][input_column.-1].strip.empty?   
+    #end
 
-    def validate_move
-        case turn
-        when 1 puts "X"
-        when 2 puts "int"
-    end        
-    
-    def update_board
-        @board[inputs[0]-1][inputs[1].-1] = @player
-
-    end
+    def valid_move? 
+        if @rol == 1
+            @draw = "X"
+        else
+            @draw = "O"
+        end
+        if @board[@cell-1] == @draw
+            puts "The cell is already taken"
+            play
+        end
+        case @cell
+            when 1
+                @board[0] = @draw
+            when 2
+                @board[1] = @draw
+            when 3
+                @board[2] = @draw
+            when 4
+                @board[3] = @draw
+            when 5
+                @board[4] = @draw
+            when 6
+                @board[5] = @draw
+            when 7
+                @board[6] = @draw
+            when 8
+                @board[7] = @draw
+            when 9
+                @board[8] = @draw
+            else
+                puts "Ivalid Option, please try again!"
+        end
+        
+        winner?
+        game_over?
+    end  
     
     def play
-        inputs = get_inputs
-        update_board(inputs)
-
+        #inputs = get_inputs
+        #update_board(inputs)
+        puts "\nPlayer #{@rol} is playing!"
+        puts "Select the cell with a number from 1 to 9"
+        @cell = gets.chomp.to_i
+        valid_move?
     end
     
-    def game_over
+    def winner?
+        
+        #Horizontal Test
+        
+        if @board[0] === @draw && @board[1] === @draw && @board[2] === @draw
+            @board[0] = "-" 
+            @board[1] = "-" 
+            @board[2] = "-"
+            print_board
+            puts "\nPlayer #{@rol} Wins"
+            again?
+        elsif @board[3] === @draw && @board[4] === @draw && @board[5] === @draw
+            @board[3] = "-" 
+            @board[4] = "-" 
+            @board[5] = "-"
+            print_board
+            puts "\nPlayer #{@rol} Wins"
+            again?
+        elsif @board[6] === @draw && @board[7] === @draw && @board[8] === @draw
+            @board[6] = "-" 
+            @board[7] = "-" 
+            @board[8] = "-"
+            print_board
+            puts "\nPlayer #{@rol} Wins"
+            again?
+        end
+        
+        #Vertical Test
+        
+        if @board[0] === @draw && @board[3] === @draw && @board[6] === @draw
+            @board[0] = "|" 
+            @board[3] = "|" 
+            @board[6] = "|"
+            print_board
+            puts "\nPlayer #{@rol} Wins"
+            again?
+        elsif @board[1] === @draw && @board[4] === @draw && @board[7] === @draw
+            @board[1] = "|" 
+            @board[4] = "|" 
+            @board[7] = "|"
+            print_board
+            puts "\nPlayer #{@rol} Wins"
+            again?
+        elsif @board[2] === @draw && @board[5] === @draw && @board[8] === @draw
+            @board[2] = "|" 
+            @board[5] = "|" 
+            @board[8] = "|"
+            print_board
+            puts "\nPlayer #{@rol} Wins"
+            again?
+        end
+        
+        #Diagonal 1 Test
+        
+        if @board[0] === @draw && @board[4] === @draw && @board[8] === @draw
+            @board[0] = "\\" 
+            @board[4] = "\\" 
+            @board[8] = "\\"
+            print_board
+            puts "\nPlayer #{@rol} Wins"
+            again?
+        end
+        
+        #Diagonal 2 Test
+        
+        if @board[2] === @draw && @board[4] === @draw && @board[6] === @draw
+            @board[2] = "/" 
+            @board[4] = "/" 
+            @board[6] = "/"
+            print_board
+            puts "\nPlayer #{@rol} Wins"
+            again?
+        end
     end
+    
+    def game_over?
+        empty = 0
+        @board.each do |cell| 
+            empty += 1 if cell == @draw
+        end
+        if empty == 9
+            print_board
+            puts "Game Over"
+            again?
+        elsif winner?
+            print_board
+        else
+            if @rol == 2 
+                @rol = 1 
+            else
+                @rol=2 
+            end
+            print_board
+            play
+        end
+    end
+    
+    def again?
+        puts "##################"
+        puts "#PLAY AGAIN? (y/n)"
+        puts "##################"
+        again = gets.chomp
+            if again == "y" 
+                @board=["1","2","3","4","5","6","7","8","9"]
+                get_inputs 
+            else 
+                puts "GoodBye!" 
+            end
+    end
+    
+   
 end
 
 game = TicTacToe.new(["1","2","3","4","5","6","7","8","9"])
